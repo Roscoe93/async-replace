47282ee89e73420931b7c84e7d2c09512f7a5249
张一丁 <zhangyiding@corp.netease.com>
2017-12-25 15:44:15 +0800

    bug fix
    遗漏最后一个字符串

--- index.js
+++ index.js
@@ -1,40 +1,42 @@
 const replaceAsync = (str, regExp, replacer) => {
-  return new Promise((resolve, reject) => {
-    if (Object.prototype.toString.call(regExp) !== "[object RegExp]") {
-      throw new Error('regExp is expected to be regExp instance!');
-    }
-    if (typeof str !== 'string') {
-      throw new Error('str is expected to be string!');
-    }
-    let strIdx = 0;
-    let replacements = [];
-    let otherParts = [];
-    let ret = '';
-    if (!regExp.global) {
-      let match = str.match(regExp);
-      replacer.call(str, match).then(res => {
-        return resolve(str.replace(regExp, res));
+  if (Object.prototype.toString.call(regExp) !== "[object RegExp]") {
+    throw new Error("regExp is expected to be regExp instance!");
+  }
+  if (typeof str !== "string") {
+    throw new Error("str is expected to be string!");
+  }
+  let strIdx = 0;
+  let replacements = [];
+  let otherParts = [];
+  let ret = "";
+  if (!regExp.global) {
+    let match = str.match(regExp);
+    replacer
+      .call(str, match)
+      .then(res => {
+        return Promise.resolve(str.replace(regExp, res));
+      })
+      .catch(e => {
+        throw e;
       });
+  } else {
+    let res;
+    while ((res = regExp.exec(str))) {
+      replacements.push(replacer.call(str, res));
+      otherParts.push(str.slice(strIdx, res.index));
+      strIdx = res.index + res[0].length;
     }
-    else {
-      while (res = regExp.exec(str)) {
-        replacements.push(replacer.call(str, res));
-        otherParts.push(str.slice(strIdx, res.index));
-        strIdx = res.index + res[0].length;
+    otherParts.push(str.slice(strIdx, str.length));
+    let i = 0;
+    return Promise.all(replacements).then(replaceRes => {
+      for (; i < replaceRes.length; i++) {
+        ret += otherParts[i];
+        ret += replaceRes[i];
       }
-      Promise.all(replacements)
-        .then(replaceRes => {
-          for (let i = 0; i < replaceRes.length; i++) {
-            ret += otherParts[i]
-            ret += replaceRes[i]
-          }
-          resolve(ret);
-        })
-        .catch(e => {
-          return reject(e);
-        });
-    }
-  });
+      ret += otherParts[i++];
+      return ret;
+    });
+  }
 };
 
 module.exports = replaceAsync;
